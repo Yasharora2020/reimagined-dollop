1fd51f288d096736b434a174ccc1602822573279
Yash <yashvardhanarora@gmail.com>
2022-02-28 22:47:53 +1100

    added player and enemy class

--- game.py
+++ game.py
@@ -1,14 +1,72 @@
 import pygame
-from pygame.locals import (K_UP,K_DOWN,K_LEFT,K_RIGHT,K_ESCAPE,KEYDOWN,QUIT,K_SPACE)
+from pygame.locals import (RLEACCEL,K_UP,K_DOWN,K_LEFT,K_RIGHT,K_ESCAPE,KEYDOWN,QUIT,K_SPACE)
+import random
 
-pygame.init()
 
-WIDTH = 800
+WIDTH = 600
 HEIGHT = 600
 
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('Game')
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.image.load("jet.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
 
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -1)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 1)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-1, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(1, 0)
+
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.image.load("missile.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        #self.surf = pygame.Surface((20, 10))
+        #self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center=(random.randint(WIDTH + 20, WIDTH + 100),random.randint(0, HEIGHT),))
+        self.speed = random.randint(1, 2)
+
+
+    # Move the sprite based on speed
+    # Remove the sprite when it passes the left edge of the screen
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
+pygame.init()
+pygame.display.set_caption('Game')
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+player = Player()
+
+
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+# - all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -24,18 +82,36 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
-    surface = pygame.Surface((50, 50))
-    surface.fill((0, 0, 0))
-    rect = surface.get_rect()
-    #screen.blit(surface, (WIDTH/2, HEIGHT/2))
-    surf_center = (
-    (WIDTH-surface.get_width())/2,
-    (HEIGHT-surface.get_height())/2
-)
+        
+        # Add a new enemy?
+        elif event.type == ADDENEMY:
+            # Create the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
 
+    # Get the set of keys pressed and check for user input
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    # Update enemy position
+    enemies.update()
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    screen.fill((0, 0, 249))
     # Draw surf at the new coordinates
-    screen.blit(surface, surf_center)
+    screen.blit(player.surf, player.rect) 
+
+    # Draw all sprites
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    # Check if any enemies have collided with the player
+    if pygame.sprite.spritecollideany(player, enemies):
+    # If so, then remove the player and stop the loop
+        player.kill()
+        running = False
     
     pygame.display.flip()
     
